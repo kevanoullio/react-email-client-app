44a87b648d92c53ffc2b37b5575d005bee444c7b
Kevin Ulliac <kevanoullio@outlook.com>
2024-01-27 15:32:35 -0700

    Added column component and css code, refactored App.js main code.

--- src/App.js
+++ src/App.js
@@ -13,47 +13,50 @@ function App() {
 				<h1>Really Cool Email Client</h1>
 			</header>
 			<main>
-				<aside className="App-sidebar">
+				<Column className="App-sidebar">
 					<Sidebar />
-				</aside>
-				<section className="App-email-index">
-					<SearchBar
-						placeholder="Search for emails"
-						handleChange={(e) => console.log(e.target.value)}
-					/>
-					<EmailList
-						emails={[
-							{
-								id: 1,
-								from: "Joe",
-								subject: "Hello",
-								address: "address",
-								timestamp: "timestamp"
-							},
-							{
-								id: 2,
-								from: "Joe",
-								subject: "Hello",
-								address: "address",
-								timestamp: "timestamp"
-							},
-							{
-								id: 3,
-								from: "Joe",
-								subject: "Hello",
-								address: "address",
-								timestamp: "timestamp"
-							}
-						]}
-					/>
-				</section>
-				<section className="App-email-body">
-					from: "Joe",
-					subject: "Hello",
-					address: "address",
-					timestamp: "timestamp",
-					message: "message"
-				</section>
+				</Column>
+				<Column className="App-email-index">
+					<EmailIndex />
+						<SearchBar
+							placeholder="Search for emails"
+							handleChange={(e) => console.log(e.target.value)}
+						/>
+						<EmailList
+							emails={[
+								{
+									id: 1,
+									from: "Joe",
+									subject: "Hello",
+									address: "address",
+									timestamp: "timestamp"
+								},
+								{
+									id: 2,
+									from: "Joe",
+									subject: "Hello",
+									address: "address",
+									timestamp: "timestamp"
+								},
+								{
+									id: 3,
+									from: "Joe",
+									subject: "Hello",
+									address: "address",
+									timestamp: "timestamp"
+								}
+							]}
+						/>
+				</Column>
+				<Column className="App-email-body">
+					<EmailBody>
+						from: "Joe",
+						subject: "Hello",
+						address: "address",
+						timestamp: "timestamp",
+						message: "message"
+					</EmailBody>
+				</Column>
 			</main>
 			<footer>
 				<p>Thank you for choosing Really Cool Email Client! 2024 &copy Copyright</p>
---
+++ src/components/column/column.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Column = ({ children }) => (
+    <section className="column">
+        {children}
+    </section>
+);
+
+export default Column;
